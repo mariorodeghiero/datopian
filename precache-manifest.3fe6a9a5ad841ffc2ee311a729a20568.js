19a068c3d4c2431c5f1f8798f74dd2f04183ff10
Mário Antônio do Amaral Rodeghiero <mario@vanhack.com>
2020-02-03 00:00:58 -0300

    Updates

---
+++ precache-manifest.3fe6a9a5ad841ffc2ee311a729a20568.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad358878a7a64f54150fbf5530490f1f",
+    "url": "/datopian/index.html"
+  },
+  {
+    "revision": "e5a228b95fb57ea95cb6",
+    "url": "/datopian/static/js/2.3af7afb9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/datopian/static/js/2.3af7afb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d83fc39e0d42a377394",
+    "url": "/datopian/static/js/main.8e7efccc.chunk.js"
+  },
+  {
+    "revision": "3c255b20576ad485753c",
+    "url": "/datopian/static/js/runtime-main.c8adc897.js"
+  },
+  {
+    "revision": "20818a70869160ebe51ec7864691b16e",
+    "url": "/datopian/static/media/dailyPrices.20818a70.csv"
+  },
+  {
+    "revision": "687cce1c91377b27a36bc6a1570e546d",
+    "url": "/datopian/static/media/datopian-logo.687cce1c.png"
+  }
+]);
